ebe8132f33c6e356f9c9d30fbc52aea5cdcbaf46
satyam <satyamsonker1010@gmail.com>
2023-07-20 23:32:37 +0530

    article summarise work complete

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -6,6 +6,7 @@ import { useLazyGetSummaryQuery } from "../services/article";
 const Demo = () => {
   const [article, setArticle] = useState({ url: "", summary: "" });
   const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
   useEffect(() => {
@@ -31,6 +32,13 @@ const Demo = () => {
       localStorage.setItem("articles", JSON.stringify(updateAllArticles));
     }
   };
+
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       {/* Search */}
@@ -60,25 +68,29 @@ const Demo = () => {
           </button>
         </form>
         {/* Browse URL History  */}
-        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
-          {allArticles.map((item, index) => {
-            <div
-              key={`link-${index}`}
-              onClick={() => setArticle(item)}
-              className="link_card"
-            >
-              <div className="copy_btn">
-                <img
-                  src={copy}
-                  alt="copy_icon"
-                  className="w-[40%] h-[40%] object-contain"
-                />
+        <div
+          className="flex flex-col gap-1 max-h-60 overflow-y-auto"
+          id="history"
+        >
+          {allArticles.length >= 0 &&
+            allArticles.map((item, index) => (
+              <div
+                key={`link-${index}`}
+                onClick={() => setArticle(item)}
+                className="link_card"
+              >
+                <div className="copy_btn" onClick={() => handleCopy(item.url)}>
+                  <img
+                    src={copied === item.url ? tick : copy}
+                    alt="copy_icon"
+                    className="w-[40%] h-[40%] object-contain"
+                  />
+                </div>
+                <p className="flex font-satoshi text-blue-700 font-medium text-sm truncate">
+                  {item.url}
+                </p>
               </div>
-              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
-                {item.url}
-              </p>
-            </div>;
-          })}
+            ))}
         </div>
       </div>
       {/* Display Results */}
